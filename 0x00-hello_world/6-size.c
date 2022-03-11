91efb6df17be385e19fe2c7d8510edfb6318b0dd
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-10 16:57:30 -0800

    Check size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,11 @@
  */
 int main(void)
 {
-	int i;
-	char c;
-	float f;
-	long int li;
-	long long int l;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of an float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of an float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
