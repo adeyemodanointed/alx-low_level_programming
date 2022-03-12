728b26aab6c6a2e4ade0f0cb1fb48db002c23b5b
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-12 13:15:18 -0800

    slight correction to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,11 @@ int main(void)
 	while (n <= 9)
 	{
 		putchar(n % 10 + '0');
-		putchar(',');
-		putchar(' ');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
 	putchar('\n');
