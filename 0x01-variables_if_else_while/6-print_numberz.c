792b185e4e26bb810c64359471de4fe046a40dec
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-12 12:40:45 -0800

    Sixth task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - print all alphabets function.
+*
+*Return: (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
