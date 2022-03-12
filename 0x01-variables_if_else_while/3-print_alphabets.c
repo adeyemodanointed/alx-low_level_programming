63b3ef05e1e7468eb0737a3326094c332c457720
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-11 16:43:24 -0800

    Print upper anc lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - print all alphabets function.
+*
+*Return: (success)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	char C = 'A';
+
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
+}
