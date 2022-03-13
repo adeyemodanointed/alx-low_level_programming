ba733394a11d3d70e83ff6512f56adf543cddcf7
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-12 16:33:07 -0800

    betty corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print to standard error
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fprinf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");	
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+			58,
+			1, stderr);
 	return (1);
 }
