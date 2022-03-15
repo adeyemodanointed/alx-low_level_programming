343ccb1c099c65ff0b3b7e05481cb2529d6ac355
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-15 14:17:04 -0700

    _putchar still testing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * Main - print _putchar
+ * main - print _putchar
  *
- *Return 0 - Success
+ * Return: Success
 */
-int main()
+int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
-	
+
 	sz = sizeof(str) / sizeof(int);
-	
+
 	for (count = 0; count < sz ; count++)
 	{
 	_putchar(str[count]);
