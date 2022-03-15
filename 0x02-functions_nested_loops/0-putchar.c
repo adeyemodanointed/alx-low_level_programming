251f919efc6355cea2a037f8eb36acda188e8db1
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-15 14:08:37 -0700

    _putchar still testing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,16 @@
 */
 int main()
 {
-	char *str[8] = {"_", "p", "u", "t", "c", "h", "a", "r"};
-	int i = 0;
-
-	while(i < 8)
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+	
+	sz = sizeof(str) / sizeof(int);
+	
+	for (count = 0; count < sz ; count++)
 	{
-		_putchar(str[i]);
+	_putchar(str[count]);
 	}
 
-	return 0;
+	_putchar('\n');
+	return (0);
 }
