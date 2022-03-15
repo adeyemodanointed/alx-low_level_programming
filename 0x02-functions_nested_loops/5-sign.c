553ee29ddd0282bf348affb3209b588a2bbd6d24
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-15 15:48:39 -0700

    retunr absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,7 +16,7 @@ return (1);
 }
 else if (n < 0)
 {
-_putchar('-')
+_putchar('-');
 return (-1);
 else
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - print absolute value
+ *
+ * @n: single integer
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+
+if (n >= 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (n * (-1));
+}
