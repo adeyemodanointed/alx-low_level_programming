ecaf913b26cf9dd7453b20d8b043d50ba1b6d3aa
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-15 17:41:31 -0700

    9 times table correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,25 @@
  */
 void times_table(void)
 {
-int i = 0, j = 0;
+int i = 0;
 
 while (i < 10)
 {
+	int j = 0;
+
 	while (j < 10)
 	{
 	int y = i * j;
-
+	
+	if (y > 10)
+	{
+	_putchar(y / 10);
+	_putchar(y % 10);
+	}
+	else
+	{
 	_putchar(y);
+	}
 	if (j != 9)
 	{
 	_putchar(',');
