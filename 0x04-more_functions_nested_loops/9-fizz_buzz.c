203b702da7e025429aa88ecea6a7829ecfc096e3
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 14:18:01 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,22 @@ int main(void)
 for (int i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
+{
 printf("FizzBuzz ");
+}
 else if (i % 5 == 0)
+{
 printf("Buzz ");
+}
 else if (i % 3 == 0)
+{
 printf("Fizz ");
+}
 else
+{
 printf("%d ", i);
+}
+    
 }
 
 return (0);
