3f855ac965a3633c462e417d317741756dfc3d5a
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 11:54:48 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,7 +7,7 @@
  *
  * Return: absolute value
  */
-int add(int a, int b)
+int mul(int a, int b)
 {
 
 return (a * b);
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: Always void.
+ */
+void print_numbers(void)
+{
+int i = 48;
+while (i <= 57)
+{
+_putchar(i);
+i++;
+}
+
+_putchar('\n');
+return;
+}
