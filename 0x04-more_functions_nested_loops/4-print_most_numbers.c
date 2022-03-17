59b408dfcb5e87e33f3f28b541c20143b8affd69
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 12:43:55 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
 void print_most_numbers(void)
 {
 int i = 48;
+
 while (i <= 57)
 {
 if (!(i == 50 || i == 52))
 {
 _putchar(i);
-i++;
 }
+i++;
 }
 
 _putchar('\n');
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - prints 1 - 14 10 times
+ *
+ * Return: Always void.
+ */
+void more_numbers(void)
+{
+int j = 0;
+
+while (j < 10)
+{
+int i = 0;
+
+while (i <= 14)
+{
+if (i > 9)
+{
+_putchar(i / 10 + '0');
+}
+_putchar(i % 10 + '0');
+i++;
+}
+
+putchar('\n');
+j++;
+}
+return;
+}
