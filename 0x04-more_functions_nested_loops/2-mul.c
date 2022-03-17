82effb8af336d6f57d2adbe523e55f42a3f89b73
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 11:51:28 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - print the multiplication of two numbers
+ *
+ * @a: single integer
+ * @b: single integer
+ *
+ * Return: absolute value
+ */
+int add(int a, int b)
+{
+
+return (a * b);
+
+}
