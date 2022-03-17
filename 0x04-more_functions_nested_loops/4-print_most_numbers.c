fda654786f7b7f13592ae41522f350a243ca11d4
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 12:01:53 -0700

    print most numbers apart from 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers excluding 2 and 4
+ *
+ * Return: Always void.
+ */
+void print_most_numbers(void)
+{
+int i = 48;
+while (i <= 57 && !(i == 50 || i == 52))
+{
+_putchar(i);
+i++;
+}
+
+_putchar('\n');
+return;
+}
