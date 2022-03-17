4ba069160f45bce2b1e6d16d5bb4a26f041a9598
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 12:53:22 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - prints line the length of n
+ *
+ *  @n: integer 
+ * Return: Always void.
+ */
+void print_line(int n)
+{
+int j = 0;
+
+while (j < n)
+{
+if (n == 0)
+{
+break;
+}
+
+_putchar('_');
+
+j++;
+}
+
+_putchar('\n');
+
+return;
+}
