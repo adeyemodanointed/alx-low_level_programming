9cd69cd2b8230522911cba1ae8fddf067ea821a8
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 14:51:45 -0700

    print square refresh

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,18 +8,18 @@
 void print_square(int size)
 {
 
-if (size <= 0)
-{
-_putchar('\n');
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-for (int i = 0; i < size; i++)
-{
-for (int j = 0; j < size; j++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
-return;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	return;
 }
