a4fbed77d90258ac949691931f316375741306cf
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-17 13:07:11 -0700

    correct main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void more_numbers(void);
 void print_numbers(void);
 int _isupper(int c);
 int _isdigit(int c);
-int print_line(int);
+void print_line(int);
 void print_number(int);
 void print_triangle(int);
 void print_square(int);
