b16f05cb3b57b407118b1ad6541213ca2c72e073
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-19 13:51:15 -0700

    correcting 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,20 @@
  */
 void jack_bauer(void)
 {
+int hour, min;
 
-if (n >= 0)
+for (hour = 0; hour < 24, hour++)
 {
-return (n);
+	_putchar(hour / 10 + '0');
+	_putchar(hour % 10 + '0');
+	_putchar(58);
+
+	for (min = 0; min < 60; min++)
+	{
+		_putchar(min / 10 + '0');
+		_putchar(min % 10 + '0');
+	}
 }
-else
-{
-return (n * (-1));
-}
+
+return;
 }
