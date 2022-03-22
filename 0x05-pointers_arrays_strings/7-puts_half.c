74d16a1e96d2c8637b5f3be2bd1f9bbecbe2d780
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-22 16:23:25 -0700

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,7 @@ void puts_half(char *str)
 int count = 0;
 int n = 0;
 
-while (count >= 0)
-{
-if (str[count] == '\0')
-{
-_putchar('\n');
-break;
-}
+while (str[count] != '\0')
 count++;
 }
 if (count % 2 == 0)
@@ -24,10 +18,11 @@ n = count / 2;
 else
 n = (count / 2) + 1;
 
-while (n <= count)
+while (n < count)
 {
 _putchar(str[n]);
 n++;
 }
+_putchar('\n');
 }
 
