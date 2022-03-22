04724bf2e59e56151ff776d67170abecfe6c8ed1
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-22 14:26:35 -0700

    correction to 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,16 +11,11 @@
 void _puts(char *str)
 {
 	int n = 0;
-	int i;
 
 	while (*(str + n) != '\0')
 	{
-		n++;
-	}
-
-	for (i = 0; i < n; i++)
-	{
-	_putchar(str[i]);
+	_putchar(str[n]);
+	n++;
 	}
 	_putchar('\n');
 
