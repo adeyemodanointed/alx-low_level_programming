ea333a63600ebb2e68c61f81b8fc9a180efe69b8
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-22 07:45:22 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - changes number to 98 
+ * reset_to_98 - changes number to 98
  *
+ * @n: pointer to a variable n
  * Return: Always 0.
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
