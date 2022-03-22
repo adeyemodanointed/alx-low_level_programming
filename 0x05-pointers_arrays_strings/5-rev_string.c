43955fc6076b69aa5dbef0634b0eb833d18b3be1
Adeyemo <adeyemodanointed@yahoo.com>
2022-03-22 15:26:46 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string to stdout in reverse
+ *
+ * @s: pointer to a string
+ *
+ * Return: Always void.
+ */
+
+void rev_string(char *s)
+{
+	int n = 0;
+	int i;
+
+	while (*(s + n) != '\0')
+	{
+	n++;
+	}
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
