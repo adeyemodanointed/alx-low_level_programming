10c8cc3680565d69b177ac7c55e5e5fd1b533c88
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-01 15:27:41 -0700

    Print argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - the main funtion
+* @argc: the number of arguments in argv
+* @argv: the vector array containing the arguments
+* Return: Success is 0.
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
