3b88cf2ac5ab590322c9c0b93a273d763a2d36db
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-01 15:32:44 -0700

    Print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - the main funtion
+* @argc: the number of arguments in argv
+* @argv: the vector array containing the arguments
+* Return: Success is 0.
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
