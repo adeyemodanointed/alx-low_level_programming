45e591036097a9080f8b8eb0ca315acb0324c976
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-05 13:22:35 -0700

    Create an array using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+/**
+* create_array - create an array of similar character
+* @c: the character
+* @size: the size of the array to print
+*
+* Return: pointer to char array.
+*/
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *y;
+
+if (size == 0)
+{
+return (NULL);
+}
+y = malloc(sizeof(c) * size);
+
+if (y == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < size; i++)
+{
+y[i] = c;
+}
+
+return (y);
+}
