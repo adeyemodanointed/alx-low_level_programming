49c822aea5fa070be0e31b1056e3b7b189fcd0c1
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-06 07:41:28 -0700

    Args to Str

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* argstostr - coverts main arguments to string
+* @ac: Number of arguments
+* @av: array of arguments
+* Return: array of char.
+*/
+char *argstostr(int ac, char **av)
+{
+int i, j, count = 0;
+char *str;
+    
+if (ac == 1 || av == NULL)
+return (NULL);
+
+for(i = 0; i < ac; i++)
+{
+    printf("%d", i);
+    for(j = 0; av[i][j] != '\0'; j++)
+    {
+    printf("%c", av[i][j]);
+    count++;
+    }
+}
+
+str = malloc(sizeof(char) * (count + ac));
+if (str == NULL);
+return (NULL);
+
+count = 0;
+for(i = 0; i < ac; i++)
+{
+    for(j = 0; av[i][j] != '\0'; j++)
+    {
+        str[count] = av[i][j];
+        count++;
+    }
+    str[count] = '\n';
+    count++;
+}
+return (str);
+}
