e02b6208364419865492bbd37d06c23bfb0ec8d8
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-06 06:58:24 -0700

    Function to free malloc memory to grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - frees memory allocated grid
+* @grid: the pointer to the grid
+* @height: height of grid
+* Return: void.
+*/
+void free_grid(int **grid, int height)
+{
+int i, j;
+
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
