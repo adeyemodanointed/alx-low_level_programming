99f03075652591eacb02f971c38845808f24be6b
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-06 07:46:01 -0700

    Args to Str

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,34 +10,34 @@ char *argstostr(int ac, char **av)
 {
 int i, j, count = 0;
 char *str;
-    
+
 if (ac == 1 || av == NULL)
 return (NULL);
 
-for(i = 0; i < ac; i++)
+for (i = 0; i < ac; i++)
 {
-    printf("%d", i);
-    for(j = 0; av[i][j] != '\0'; j++)
-    {
-    printf("%c", av[i][j]);
-    count++;
-    }
+printf("%d", i);
+for (j = 0; av[i][j] != '\0'; j++)
+{
+printf("%c", av[i][j]);
+count++;
+}
 }
 
 str = malloc(sizeof(char) * (count + ac));
-if (str == NULL);
+if (str == NULL)
 return (NULL);
 
 count = 0;
-for(i = 0; i < ac; i++)
+for (i = 0; i < ac; i++)
 {
-    for(j = 0; av[i][j] != '\0'; j++)
-    {
-        str[count] = av[i][j];
-        count++;
-    }
-    str[count] = '\n';
-    count++;
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[count] = av[i][j];
+count++;
+}
+str[count] = '\n';
+count++;
 }
 return (str);
-}
+}}
