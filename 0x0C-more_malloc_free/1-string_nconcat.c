52aa1f43657ea4bc5b570de07fd80b27a847ced3
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-08 07:03:41 -0700

    array range

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,48 @@
 #include <stdlib.h>
 
 /**
-* string_nconcat - concatenate string to n length
-* @s1: First string
-* @s2: Second string
-* @n: length to copy
-* Return: pointer to the concatenated string
-*/
-
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int count1, count2, count;
-char *check;
-unsigned int i;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-for (count1 = 0; s1[count1] != '\0'; count1++)
-	;
-for (count2 = 0; s2[count2] != '\0'; count2++)
-	;
+	if (s1 == NULL)
+		s1 = "";
 
-if (!n)
-count = count1;
-else if (n >= count2)
-count = count1 + count2;
-else
-count = count1 + n;
+	if (s2 == NULL)
+		s2 = "";
 
-check = malloc(sizeof(char) * (count + 1));
-if (check == NULL)
-return (NULL);
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-for (i = 0; i < count1; i++)
-check[i] = s1[i];
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-if (n > 0)
-{
-for (i = count1 ; i < count; i++)
-check[i] = s2[i - count1];
-}
-check[i] = '\0';
+	if (n > ls2)
+		n = ls2;
 
-return (check);
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
