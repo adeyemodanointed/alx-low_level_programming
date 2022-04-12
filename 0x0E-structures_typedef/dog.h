3499e1b308d8144957fabf0279e846463cda27e3
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-12 05:43:55 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H_
 #define DOG_H_
 
+/**
+ * struct dog - creates a structure for a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the god owner
+ */
+
 struct dog
 {
 	char *name;
