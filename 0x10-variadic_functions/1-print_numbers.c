cd935c9fc1dc44f9ffa08dbe93dc05976a966e3c
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-15 07:50:16 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n - 1; i++)
-	{	
+	{
 		if (!(*separator))
 		printf("%d", va_arg(args, int));
 		printf("%d%s", va_arg(args, int), separator);
