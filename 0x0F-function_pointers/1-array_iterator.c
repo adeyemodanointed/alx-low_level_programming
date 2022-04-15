e6e92b181480c3c31aebea0e98b81191e779ca76
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-15 12:27:04 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - runs all element as arguments of a function
+ * @array: array of elements
+ * @size: size of the array
+ * @action: pointer to a function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (action)(int))
+{
+	size_t i = 0;
+
+	if (!action)
+		return;
+
+	for ( ; i < size; i++)
+		(*action)(array[i]);
+}
