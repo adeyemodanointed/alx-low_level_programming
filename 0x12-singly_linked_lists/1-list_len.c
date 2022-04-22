36fa677e53ddaa6c7690a96cabaab286d9082717
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-22 07:27:02 -0700

    print number of elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* list_len - returns the number of member of a singly linked list
+* @h: pointer to the head
+* Return: length of list of type size_t
+*/
+
+size_t list_len(const list_t *h)
+{
+size_t n = 0;
+const list_t *ptr = h;
+while (ptr != NULL)
+{
+n++;
+
+ptr = ptr->next;
+}
+return (n);
+}
