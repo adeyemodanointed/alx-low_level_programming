8ba457b0eb1d7fd4245ff859fa217faa72c368ff
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-22 19:05:37 -0700

    Add element to end of a linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+* add_node_end - add an element to the end of a linked list
+* @head: pointer to the head
+* @str: the string to be copied
+* Return: the created element address
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *ptr;
+list_t *newNode = malloc (sizeof(list_t));
+
+if (!newNode)
+return (NULL);
+
+newNode->str = strdup(str);
+newNode->len = strlen(str);
+newNode->next = NULL;
+
+if (*head == NULL)
+{
+*head  = malloc (sizeof(list_t));
+if (!(*head))
+return (NULL);
+*head = newNode;
+}
+else {
+ptr = *head;
+while (ptr->next != NULL)
+{
+ptr = ptr->next;
+}
+ptr->next = newNode;
+}
+
+return (newNode);
+}
