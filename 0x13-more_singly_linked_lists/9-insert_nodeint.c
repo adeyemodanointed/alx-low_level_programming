ad663942b9a7eee1120ee545c53a427ffb588918
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-27 14:06:37 -0700

    minor changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr;
 	unsigned int i = 0;
 	listint_t *temp = malloc(sizeof(listint_t));
+
 	if (!temp)
 		return (NULL);
 	ptr = *head;
 
-	while (i < (idx - 1))
+	while (i < (idx - 1) && ptr != NULL)
 	{
 		ptr = ptr->next;
 		i++;
