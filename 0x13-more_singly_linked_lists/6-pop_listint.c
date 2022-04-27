d3bacb83dbf09e73481d436554b78c6cd5736fea
Adeyemo <adeyemodanointed@yahoo.com>
2022-04-27 12:17:58 -0700

    minor change

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
 	if (*head == NULL)
 		return (0);
 
 	temp = *head;
 	*head = temp->next;
+	n = temp->n;
 	free(temp);
 
-	return (temp->n);
+	return (n);
 }
