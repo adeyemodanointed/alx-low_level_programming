3bd86df2ba5b71d6a0f62842599e9380aed845fb
Adeyemo <adeyemodanointed@yahoo.com>
2022-05-04 14:48:13 -0700

    minor change

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <unistd>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
