5ef2c139e6696265b9d34ccca0de9e8462c3fe56
Adeyemo <adeyemodanointed@yahoo.com>
2022-06-09 14:25:35 -0700

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints the list of all values in a doubly linked list
+ * dlistint_len - returns number of element in a doubly linked list
  * @h: head of the list
  *
  * Return: the size of the list
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count;
 
 	for (count = 0; h != NULL; count++)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 	}
 
